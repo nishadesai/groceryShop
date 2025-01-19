62057f0c6c27a08727131418ef3346e3a742345e
desai <desai@atb-bremen.de>
2025-01-19 15:03:34 +0100

    Prepare for GitHub Pages deployment1

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/groceryShop/', // Update with your repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
